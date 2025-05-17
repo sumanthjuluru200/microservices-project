971db1b8cac808045ae52ea3183b3ce0756aa690
sumanthjuluru200 <sumanthjuluru200@gmail.com>
2025-05-17 16:54:22 +0530

    Loans microservices

---
+++ loans/src/main/java/com/banking/loans/entity/Loans.java
@@ -0,0 +1,35 @@
+package com.banking.loans.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Loans extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loanId;
+
+    private String mobileNumber;
+
+    private String loanNumber;
+
+    private String loanType;
+
+    private int totalLoan;
+
+    private int amountPaid;
+
+    private int outstandingAmount;
+
+}
